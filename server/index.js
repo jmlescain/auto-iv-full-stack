dfb4c5221628ebbfbd73f62011968ac420154d95
Jan Lescain <jmlescain@gmail.com>
2019-10-14 16:18:25 +0800

    change density to 1

--- server/index.js
+++ server/index.js
@@ -181,8 +181,8 @@ if (!isDev && cluster.isMaster) {
       if (weight <= 30) {
         weight = -1
       } else {
-        //weight = (weight - 90 );
-        weight = (weight - 90 )* 0.946969697;
+        weight = (weight - 90 );
+        //weight = (weight - 90 )* 0.946969697;
         if (weight > 500) weight = 500;
         weight = Math.trunc(weight);
       }
